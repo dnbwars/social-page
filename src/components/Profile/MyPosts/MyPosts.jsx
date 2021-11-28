bac33dba1dc109b18af4b0c5abeb2c740b8034b9
dnbwars <evgeniikasian@gmail.com>
2021-11-28 17:14:08 +0200

    added new logic and components

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,23 +6,35 @@ const MyPosts = (props) => {
 
   let postsElements = props.posts.map (p => <Post message={p.message} likeCount={p.likeCount} /> );
 
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  }
+
   return (
     <div>
       <h3>My post</h3>
-      <div className={s.posts}>
+      <div className={s.posts}  >
         <div>
-          <textarea></textarea>
+          <textarea ref="{newPostElement"></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={ addPost }>Add post</button>
         </div>
       </div>
       <div className={s.posts}>
-        { postsElements }
+        {postsElements}
       </div>
     </div>
   );
 };
 
+function HandleClick() {
+  console.log('---', 'clicked')
+}
+
+//получаем из index => app => массив постов с лайками и сообщением через пропсы
 export default MyPosts;
  
